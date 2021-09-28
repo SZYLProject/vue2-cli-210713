85a46a40ba013eebe058ab6450974efb2ab8e4dd
baiam <baiam@dchealth.com>
2021-09-28 17:45:38 +0800

    【添加散点图】

--- src/components/echarts/index.vue
+++ src/components/echarts/index.vue
@@ -63,7 +63,11 @@
         <!-- 折线图 -->
         <el-row>
           <el-col :span="12">
-            <StackedAreaChart title="StackedAreaChart" height="400px" width="100%" />
+            <StackedAreaChart
+              title="StackedAreaChart"
+              height="400px"
+              width="100%"
+            />
           </el-col>
           <el-col :span="12">
             <StepLine
@@ -77,7 +81,11 @@
         <!-- 雷达图 -->
         <el-row>
           <el-col :span="12">
-            <CustomizedRadarChart title="CustomizedRadarChart" height="400px" width="100%" />
+            <CustomizedRadarChart
+              title="CustomizedRadarChart"
+              height="400px"
+              width="100%"
+            />
           </el-col>
         </el-row>
         <!-- 何须图 -->
@@ -91,6 +99,29 @@
               labelPosition="center"
               id="main"
               :data="pieData"
+            />
+          </el-col>
+        </el-row>
+        <!-- 散点图 -->
+        <el-row>
+          <el-col :span="12">
+            <LinearRegression
+              height="400px"
+              width="100%"
+              id="RelatedAnalysisLinearRegression"
+              :titleShow="true"
+              title="Smoking history在不同Gender分组中的堆积柱形图"
+              :LinearRegressionData="scatterData"
+            />
+          </el-col>
+          <el-col :span="12">
+            <BasicScatterChart
+              height="400px"
+              width="100%"
+              id="RelatedAnalysisBasicScatterChart"
+              :titleShow="true"
+              title="Smoking history在不同Gender分组中的堆积柱形图"
+              :BasicScatterChartData="scatterData"
             />
           </el-col>
         </el-row>
@@ -109,6 +140,8 @@ import NestedPies from "@/components/echarts/Pie/NestedPies";
 import RefererOfAWebsite from "@/components/echarts/Pie/RefererOfAWebsite";
 import CustomizedRadarChart from "@/components/echarts/Radar/CustomizedRadarChart";
 import VerticalAxisBar from "@/components/echarts/Bar/VerticalAxisBar";
+import BasicScatterChart from "@/components/echarts/ScatterPlot/BasicScatterChart";
+import LinearRegression from "@/components/echarts/ScatterPlot/LinearRegression";
 
 export default {
   components: {
@@ -121,10 +154,29 @@ export default {
     NestedPies,
     RefererOfAWebsite,
     CustomizedRadarChart,
-    VerticalAxisBar
+    VerticalAxisBar,
+    BasicScatterChart,
+    LinearRegression
   },
   data() {
     return {
+      scatterData: [
+        [-1.067732, -1.176513],
+        [-0.42781, -1.0816464],
+        [0.995731, 4.550095],
+        [0.738336, 4.256571],
+        [0.981083, 4.560815],
+        [0.526171, 3.929515],
+        [0.378887, 3.52617],
+        [0.033859, 3.156393],
+        [0.132791, 3.110301],
+        [0.138306, 3.149813],
+        [0.247809, 3.476346],
+        [0.64827, 4.119688],
+        [0.731209, 4.282233],
+        [-0.236833, -1.0486582],
+        [0.969788, 4.655492]
+      ],
       pieData: [
         { value: 435, name: "是" },
         { value: 310, name: "否" }
